a0d4b4faae26730301e4744379da52da2199f496
mbela mbemba <mbela.mbemba@gmail.com>
2019-01-17 10:18:25 +0100

    withdrawal credit update

--- controller/bankAccountController.php
+++ controller/bankAccountController.php
@@ -7,12 +7,57 @@ require "model/dataBase.php";
  */
 class bankAccountController
 {
+  public function showBankAccounts(){
+    $bankAccountManager = new bankAccountManager();
+    $bankAccounts = $bankAccountManager->getAccounts();
+    require "view/bankAccountsView.php";
+  }
+
   public function showBankAccount(){
     $bankAccountManager = new bankAccountManager();
-    $bankAccount = $bankAccountManager->getAccounts();
-    var_dump($bankAccount);
+    $bankAccounts = $bankAccountManager->getAccount();
     require "view/bankAccountView.php";
   }
+
+  public function makeWithdrawal(){
+     if(!empty($_POST)){
+       $bankAccountManager = new bankAccountManager();
+       $id = intval($_GET['id']);
+       $bankAccountManagerOK = $bankAccountManager->getAccount($_GET['id']);
+       $bankAccountManagerOK = $bankAccountManagerOK[0];
+       $balance = $bankAccountManagerOK->getBalance();
+       if($bankAccountManagerOK->withdrawal(intval($_POST["amount"]))){
+         $newBalance = $balance - $_POST["amount"];
+         $bankAccountManagerOK->setBalance($newBalance);
+         $bankAccountManager->updateAccount($bankAccountManagerOK);
+         redirectTo("bankAccounts");
+       }
+       echo "Vous avez atteint le decouvert autorisé!";
+     }
+     require "view/form/withdrawalAccountForm.php";
+  }
+
+  public function makeCredit(){
+
+
+     if(!empty($_POST)){
+       $bankAccountManager = new bankAccountManager();
+       $id = intval($_GET['id']);
+       $bankAccountManagerOK = $bankAccountManager->getAccount($_GET['id']);
+       $bankAccountManagerOK = $bankAccountManagerOK[0];
+       $balance = $bankAccountManagerOK->getBalance();
+       $newBalance = $balance + $_POST["amount"];
+       $bankAccountManagerOK->setBalance($newBalance);
+       $bankAccountManager->updateAccount($bankAccountManagerOK);
+       redirectTo("bankAccounts");
+    }
+    require "view/form/creditAccountForm.php";
+  }
+
+  public function makeTransfert(){
+    require "view/form/transfertAccountForm.php";
+  }
+
 
 }
 
---
+++ view/form/creditAccountForm.php
@@ -0,0 +1,12 @@
+
+<div class="container col-6">
+<!-- form action a remplir quand toutes les pages seront crées-->
+    <h2>Crediter Le compte</h2>
+    <form method="post">
+        <div class="form-group">
+          <label for="amount">Montant à créditer</label>
+          <input type="text" class="form-control" id="amount" name="amount" placeholder="" >
+        </div>
+        <button type="submit" class="btn btn-success">Créditer</button>
+    </form>
+</div>
